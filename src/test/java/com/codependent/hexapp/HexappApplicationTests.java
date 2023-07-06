da1f3e2bac424f5a278999cb3cef1e9fe326e50a
jainigo <joseantonio.inigo@gmail.com>
2023-07-06 15:22:28 +0200

    mutation testing 100% coverage

--- src/test/java/com/codependent/hexapp/HexappApplicationTests.java
+++ src/test/java/com/codependent/hexapp/HexappApplicationTests.java
@@ -2,12 +2,25 @@ package com.codependent.hexapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestConstructor;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
+
+@TestConstructor(autowireMode = ALL)
 @SpringBootTest
 class HexappApplicationTests {
+    
+    private final ApplicationContext applicationContext;
+
+    HexappApplicationTests(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
