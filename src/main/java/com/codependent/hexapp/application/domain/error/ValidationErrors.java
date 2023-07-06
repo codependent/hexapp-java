4f94ceda6e43b8564b3f7f4b194fafb24fe69b24
jainigo <joseantonio.inigo@gmail.com>
2023-07-06 13:49:35 +0200

    mutation testing

--- src/main/java/com/codependent/hexapp/application/domain/error/ValidationErrors.java
+++ src/main/java/com/codependent/hexapp/application/domain/error/ValidationErrors.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 public final class ValidationErrors implements ApplicationError {
     
-    private final List<ValidationError> errors = new ArrayList<>();
+    private final List<ValidationError> errors;
+
+    public ValidationErrors() {
+        this.errors = new ArrayList<>();
+    }
     
+    public ValidationErrors(List<ValidationError> errors) {
+        this.errors = errors;
+    }
+
+
     public void add(ValidationError error) {
         errors.add(error);
     }
--- src/test/java/com/codependent/hexapp/adapter/in/web/DepartmentControllerTests.java
+++ src/test/java/com/codependent/hexapp/adapter/in/web/DepartmentControllerTests.java
@@ -1,6 +1,12 @@
 package com.codependent.hexapp.adapter.in.web;
 
 import com.codependent.hexapp.application.domain.Department;
+import com.codependent.hexapp.application.domain.error.DepartmentBlacklistedError;
+import com.codependent.hexapp.application.domain.error.DepartmentExistsError;
+import com.codependent.hexapp.application.domain.error.InvalidField;
+import com.codependent.hexapp.application.domain.error.ValidationErrors;
+import com.codependent.hexapp.application.domain.exception.DomainErrorException;
+import com.codependent.hexapp.application.domain.exception.ValidationErrorsException;
 import com.codependent.hexapp.application.port.in.CreateDepartmentUseCase;
 import com.codependent.hexapp.application.port.in.dto.CreateDepartmentCommand;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +17,8 @@ import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.when;
@@ -42,5 +50,52 @@ class DepartmentControllerTests {
                         .contentType(APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name", is(createDepartmentCommand.name())));
+    }
+
+    @Test
+    void shouldFailCreatingInvalidDepartment() throws Exception {
+
+        InvalidField invalidField = new InvalidField("department", "id", "invalid");
+        InvalidField invalidField2 = new InvalidField("department", "name", "empty");
+
+        CreateDepartmentCommand createDepartmentCommand = new CreateDepartmentCommand(0, "");
+        when(createDepartmentUseCase.createDepartment(createDepartmentCommand))
+                .thenThrow(new ValidationErrorsException(new ValidationErrors(List.of(
+                        invalidField,
+                        invalidField2))));
+
+        mvc.perform(MockMvcRequestBuilders.post("/departments")
+                        .content(new ObjectMapper().writeValueAsString(createDepartmentCommand))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                
+    }
+
+    @Test
+    void shouldFailCreatingExistingDepartment() throws Exception {
+        
+        CreateDepartmentCommand createDepartmentCommand = new CreateDepartmentCommand(0, "");
+        when(createDepartmentUseCase.createDepartment(createDepartmentCommand))
+                .thenThrow(new DomainErrorException(new DepartmentExistsError()));
+
+        mvc.perform(MockMvcRequestBuilders.post("/departments")
+                        .content(new ObjectMapper().writeValueAsString(createDepartmentCommand))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+
+    }
+
+    @Test
+    void shouldFailCreatingBlacklistedDepartment() throws Exception {
+
+        CreateDepartmentCommand createDepartmentCommand = new CreateDepartmentCommand(0, "");
+        when(createDepartmentUseCase.createDepartment(createDepartmentCommand))
+                .thenThrow(new DomainErrorException(new DepartmentBlacklistedError()));
+
+        mvc.perform(MockMvcRequestBuilders.post("/departments")
+                        .content(new ObjectMapper().writeValueAsString(createDepartmentCommand))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+
     }
 }
