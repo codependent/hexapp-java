fa5cd5160e136277bbb2b3fbabbe553396d2cf3e
jainigo <joseantonio.inigo@gmail.com>
2023-07-17 14:16:31 +0200

    ignored withX props

--- src/main/java/com/codependent/hexapp/application/port/in/mapper/DepartmentMapper.java
+++ src/main/java/com/codependent/hexapp/application/port/in/mapper/DepartmentMapper.java
@@ -3,12 +3,15 @@ package com.codependent.hexapp.application.port.in.mapper;
 import com.codependent.hexapp.application.domain.Department;
 import com.codependent.hexapp.application.port.in.dto.CreateDepartmentCommand;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(componentModel = SPRING)
 public interface DepartmentMapper {
 
+    @Mapping(target = "withId", ignore = true) 
+    @Mapping(target = "withName", ignore = true)
     Department commandToDomain(CreateDepartmentCommand command);
     
 }
