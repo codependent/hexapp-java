45b91d2a66971f82cc60965d0bdaae5b34a94d1d
jainigo <joseantonio.inigo@gmail.com>
2023-07-17 14:17:31 +0200

    ignored unmapped

--- src/main/java/com/codependent/hexapp/application/port/in/mapper/DepartmentMapper.java
+++ src/main/java/com/codependent/hexapp/application/port/in/mapper/DepartmentMapper.java
@@ -3,15 +3,15 @@ package com.codependent.hexapp.application.port.in.mapper;
 import com.codependent.hexapp.application.domain.Department;
 import com.codependent.hexapp.application.port.in.dto.CreateDepartmentCommand;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.IGNORE;
 
-@Mapper(componentModel = SPRING)
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE)
 public interface DepartmentMapper {
 
-    @Mapping(target = "withId", ignore = true) 
-    @Mapping(target = "withName", ignore = true)
+    //@Mapping(target = "withId", ignore = true) 
+    //@Mapping(target = "withName", ignore = true)
     Department commandToDomain(CreateDepartmentCommand command);
     
 }
